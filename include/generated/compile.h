83e85ec4f9d05e87ba166bdc42da78280f770184
GruesomeWolf <boober1984@gmail.com>
2013-08-13 14:02:02 -0400

    First Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Aug 12 19:08:12 EDT 2013"
+#define LINUX_COMPILE_BY "gruesome"
+#define LINUX_COMPILE_HOST "gruesome-Inspiron-1764"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.10-gd6f147e"
